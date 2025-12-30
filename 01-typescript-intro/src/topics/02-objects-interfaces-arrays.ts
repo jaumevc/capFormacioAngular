6f402f05273ab785c9b1a5ff65397e2741e3732b
Valls Campistany <jaume.valls-campistany@capgemini.com>
2025-12-30 22:34:13 +0100

    3.13 objectes, interface i arrays

---
+++ 01-typescript-intro/src/topics/02-objects-interfaces-arrays.ts
@@ -0,0 +1,32 @@
+
+const skills: string[] =['jaume', 'susana', 'mariona', 'ares'];
+
+interface defTipus {
+    name:string;
+    hp:number;
+    altres:boolean;
+    skills:string[];
+    localitat?:string;
+};
+
+const objecte : defTipus={
+    name: 'Jaume',
+    hp:100,
+    altres: true,
+    skills:['guapo', 'patidor'] ,
+}
+
+console.log(objecte);
+
+objecte.localitat = 'Lleida';
+
+console.log(objecte);
+
+console.table(objecte);
+
+
+
+
+
+
+export {};
