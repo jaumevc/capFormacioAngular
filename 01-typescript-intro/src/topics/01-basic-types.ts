cf1987c1ca67c8ad0b4de5852a45706758e97d52
Valls Campistany <jaume.valls-campistany@capgemini.com>
2025-12-30 21:37:03 +0100

    3.12 tipus bàsics

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,21 @@
+const name: string ='Jaume';
+
+//name = '123';
+
+// let helpPoints: number | string = 123;
+
+// helpPoints= 'Full';
+
+let helpPoints: number | 'Full' | boolean= 123;
+
+//helpPoints= 'Full';
+//helpPoints= 'Jaume';
+//helpPoints = true;
+
+const isAlive : boolean = true;
+
+console.log(
+    {name, helpPoints, isAlive}
+);
+
+//export {};
