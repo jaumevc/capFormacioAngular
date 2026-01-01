711324bce18b34dcbfea0c1b3845d37d3d862767
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-01 18:52:15 +0100

    3.16 tasca tipus i funcions

---
+++ 01-typescript-intro/src/topics/04-tasca-types.ts
@@ -0,0 +1,32 @@
+/*
+    ===== Código de TypeScript =====
+*/
+interface SuperHero {
+    name: string;
+    age: number;
+    address: Adress;
+    showAddress: ()=> string;
+};
+
+interface Adress {
+    calle: string;
+    pais: string;
+    city: string;
+};
+
+const superHeroe : SuperHero = {
+    name: 'Spiderman',
+    age: 30,
+    address: {
+        calle: 'Main St',
+        pais: 'USA',
+        city: 'NY'
+    },
+    showAddress() {
+        return this.name + ', ' + this.address.city + ', ' + this.address.pais;
+    }
+}
+
+
+const address = superHeroe.showAddress();
+console.log( address );
