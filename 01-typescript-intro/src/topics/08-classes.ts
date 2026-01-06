965fe23b9322824570be21610c1631557ff5ed31
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-06 19:58:53 +0100

    3.25 prioritzar composició a herencia

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -24,17 +24,41 @@ const person2 = new Person('Jaume', 'Lleida', 54);
 // console.log('Person Adress:', person2.adress);
 //console.log('Person Age:', person2.age); 
 
-export class Hero extends Person{ 
-    constructor(public mot: string, public vacationalAdress: string, public money:number){
-        super(mot, 'Torredembarra', 45);
+// export class Hero extends Person{ 
+//     constructor(public mot: string, public vacationalAdress: string, public money:number){
+//         super(mot, 'Torredembarra', 45);
+//     }
+// }
+
+// export class Hero { 
+
+//     public person: Person;
+
+//     constructor(public mot: string, public vacationalAdress: string, public money:number){
+//        this.person = new Person(mot, 'Torredembarra', 45);
+//     }
+// }
+
+export class Hero { 
+    
+    //public person: Person;
+
+    constructor(
+        public mot: string, 
+        public vacationalAdress: string, 
+        public money:number,
+        public person: Person   
+    ){
+       //this.person = new Person(mot, 'Torredembarra', 45);
     }
 }
 
-const hero1 = new Hero('Peter Parker', 'New York', 21);
+const person3 = new Person('Jaume', 'Lleida', 54);
+const hero1 = new Hero('Peter Parker', 'New York', 21, person3);
 
 console.log('Hero name:', hero1.mot);
 console.log('Hero vacationalAdress:', hero1.vacationalAdress);
 console.log('Hero money:', hero1.money);
-console.log('Hero name:', hero1.name);
-console.log('Hero adress:', hero1.adress);
-console.log('Hero Age:', hero1.age);
+console.log('Hero name:', hero1.person.name);
+console.log('Hero adress:', hero1.person.adress);
+console.log('Hero Age:', hero1.person.age);
