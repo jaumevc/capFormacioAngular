99365a9142a06f54687736b378bc7a5d79d0c5d5
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-11 11:14:11 +0100

    4.35 carpeta SRC

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // /holaMon -> HolaMonComponent
+    // /admin -> AdminComponent
+    // / (ruta buida) -> IniciComponent
+    // /404 -> PageNotFoundComponent
+];
--- 02-bases/src/app/app.ts
+++ 02-bases/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root', //opcional
+  imports: [RouterOutlet],//opcional
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('02-bases');
