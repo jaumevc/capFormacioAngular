6336bcbb98d8f14bfb03dc02d1bd7b112576e9a3
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-13 16:01:31 +0100

    4.36 separar templates i styles

--- 02-bases/src/app/pages/counter/counter.component.ts
+++ 02-bases/src/app/pages/counter/counter.component.ts
@@ -1,14 +1,29 @@
 import { Component } from "@angular/core";
 
 @Component({
-    template: `
-    <h1>Counter Page</h1>
-    <h2>CounterComponent page</h2>
-    <h3>Counter: {{ counter }}</h3>
-    <button (click)="increase()">+1</button>
-    <button (click)="decrease()">-1</button>
-    <button (click)="reset()">Reset</button>
-    `
+    // template: `
+    // <h1>Counter Page</h1>
+    // <h2>CounterComponent page</h2>
+    // <h3>Counter: {{ counter }}</h3>
+    // <button (click)="increase()">+1</button>
+    // <button (click)="decrease()">-1</button>
+    // <button (click)="reset()">Reset</button>
+    // `
+    templateUrl: './counter.component.html',
+    // styles:`
+    //     button {
+    //         background-color: #007bff;
+    //         color: white;
+    //         border: none;
+    //         padding: 10px 20px;
+    //         margin: 5px;
+    //         cursor: pointer;
+    //     }
+    //     button:hover {
+    //         background-color: #0056b3;
+    //     }   
+    // `
+    styleUrl: './counter.component.css',
 })  
 export class CounterComponent {
 
