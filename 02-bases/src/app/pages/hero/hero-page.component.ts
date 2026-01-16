8fe61ed633eb403cb33106f16fccdb80a23ac349
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-16 15:00:14 +0100

    4.42 signals computades

--- 02-bases/src/app/pages/hero/hero-page.component.ts
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -1,16 +1,29 @@
-import { Component, signal } from "@angular/core";
+
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
     templateUrl: './hero-page.component.html',
+    imports: [UpperCasePipe],
 })      
 export class HeroPageComponent {
       name = signal('Ironman') ;
       age =  signal(45);
 
-      getHeroDescription(): string {
-          return `${ this.name() } - ${ this.age() } años`;
-      }
+      heroDescription = computed( () => {
+        const descripcio=`${ this.name() } - ${ this.age() } años`
+        return descripcio;
+      });
 
+    //   getHeroDescription(): string {
+    //       return `${ this.name() } - ${ this.age() } años`;
+    //   }
+
+    capitalizeName = computed( () => {
+        return this.name().toUpperCase();
+      });
+
+      
       changeHero(): void {
           this.name.set('Spiderman');
           this.age.set(30);
