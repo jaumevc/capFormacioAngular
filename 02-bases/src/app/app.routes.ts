a493d96774509d1d6f81df7454b025e138463113
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-19 20:36:15 +0100

    5.50 tasca routerLink

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { count } from 'rxjs';
 import { CounterComponent } from './pages/counter/counter.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 
 export const routes: Routes = [
    { path: '', component: CounterComponent },
    { path: 'hero', component: HeroPageComponent }, 
+   { path: 'dragonball', component: DragonballPageComponent }, 
    //{ path: '**', component: CounterComponent },
    { path: '**', redirectTo: '' },
 ];
---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+
+
+
+@Component({
+  imports: [],
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css',
+})
+export class DragonballPageComponent {
+
+}
