eb8a2868ad751d497e0d106c7b9c5991150940f8
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-20 17:53:52 +0100

    5.50 @for

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,7 +1,10 @@
-import { Component} from '@angular/core';
+import { Component, signal} from '@angular/core';
 
-
-
+interface Character { 
+  id:number;
+  name: string;
+  power: number;
+}
 
 @Component({
   imports: [],
@@ -9,5 +12,13 @@ import { Component} from '@angular/core';
   styleUrl: './dragonball-page.component.css',
 })
 export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8500 },
+    { id: 3, name: 'Gohan', power: 7000 },
+    { id: 4, name: 'Piccolo', power: 6500 },
+    { id: 5, name: 'Frieza', power: 12000 },
+  ]);
 
 }
