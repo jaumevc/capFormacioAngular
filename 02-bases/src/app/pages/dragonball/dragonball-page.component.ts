f64a9fbfab3ee4ab178fc21761b902518f177088
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-20 22:47:40 +0100

    5.52 ngClass. Classes CSS condicionals

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal} from '@angular/core';
+import { /*NgClass,*/ NgStyle } from '@angular/common';
+import { Component, computed, signal} from '@angular/core';
 
 interface Character { 
   id:number;
@@ -7,7 +8,7 @@ interface Character {
 }
 
 @Component({
-  imports: [],
+  imports: [/*NgClass,*/NgStyle],
   templateUrl: './dragonball-page.component.html',
   styleUrl: './dragonball-page.component.css',
 })
@@ -20,5 +21,36 @@ export class DragonballPageComponent {
     { id: 4, name: 'Piccolo', power: 6500 },
     { id: 5, name: 'Frieza', power: 12000 },
   ]);
+
+//   powerClasses = computed(() => {
+//     const chars = this.characters();
+//     const classes: string[] = new Array(chars.length);
+//     for (let i = 0; i < chars.length; i++) {
+//       classes[i] = chars[i].power < 7000 ? 'text-danger' : 'text-primary';
+//     }
+//     return classes;
+// });
+
+  // powerClasses = computed(() => {
+  //   return { 'text-primary': true };
+  // });
+
+  
+powerStyles = computed(() => {
+  const chars = this.characters();
+  const styles: Array<Record<string, string>> = new Array(chars.length);
+  for (let i = 0; i < chars.length; i++) {
+    const p = chars[i].power;
+    // Tria els colors que vulguis; aquí poso un exemple tipus Bootstrap
+    if (p < 7000) {
+      styles[i] = { color: '#dc3545', 'font-weight': '600' }; // danger = vermell
+    } else if (p <= 9000) {
+      styles[i] = { color: '#ffc107', 'font-weight': '600' }; // warning = groc/ambre
+    } else {
+      styles[i] = { color: '#0d6efd', 'font-weight': '600' }; // primary = blau
+    }
+  }
+  return styles;
+});
 
 }
