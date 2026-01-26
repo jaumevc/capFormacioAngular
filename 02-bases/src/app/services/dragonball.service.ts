21d995530379e4963dae31e0f104c4358d588ea3
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-26 17:17:58 +0100

    5.62 LINKEDSIGNAL, carregar del LOCALSTORAGE

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,16 +1,28 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+// const loadFromLocalStorage = (): Character[] => {
+//   const personatges = localStorage.getItem('personatges');
+//   return personatges ? JSON.parse(personatges) : [];
+// };
+
+// function loadFromLocalStorage(): Character[] {
+//     const personatges = localStorage.getItem('personatges');
+//     return personatges ? JSON.parse(personatges) : [];
+//   };
+
 @Injectable({
   providedIn: 'root'
 })
 export class DragonballService {
   constructor() { }
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8500 },
-  ]);
+  characters = signal<Character[]>(this.loadFromLocalStorage());
+
+//   characters = signal<Character[]>([
+//     { id: 1, name: 'Goku', power: 9001 },
+//     { id: 2, name: 'Vegeta', power: 8500 },
+//   ]);
 
    addCharacter(character: Character) {
     this.characters.update(chars => [...chars, character]);
@@ -22,5 +34,10 @@ export class DragonballService {
     localStorage.setItem('personatges', JSON.stringify(chars));
     console.log(`Quantitat de personatges: ${chars.length}`);
   });
-}
 
+  //5.62: funció per carregar personatges del localstorage
+  loadFromLocalStorage(): Character[] {
+    const personatges = localStorage.getItem('personatges');
+    return personatges ? JSON.parse(personatges) : [];
+  };
+}
