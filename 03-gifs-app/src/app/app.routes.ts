3c90bda3c0ec26b24caa1bd1c1d2a25c86fc00c5
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-28 12:50:58 +0100

    6.69 pensar en components

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'dashboard',
+        //perque es carregui de manera lazy. Nomes es carregara quan s'accedeixi a aquesta ruta.
+        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page')
+    },
+
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+
+
+
+];
