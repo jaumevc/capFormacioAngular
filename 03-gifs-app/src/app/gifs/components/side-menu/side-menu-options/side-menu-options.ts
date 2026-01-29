2ff9410bf268b073a6d0c24cca983ac865e864fc
Valls Campistany <jaume.valls-campistany@capgemini.com>
2026-01-29 18:46:25 +0100

    6.72 RouterLink des de components fills

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MenuOption } from '../../../interfaces/menu-option.interface'; 
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
   styleUrl: './side-menu-options.css',
 })
 export class SideMenuOptions {
-
+  public menuOptions: MenuOption[] = [
+    {
+      label: 'Tendències',  
+      subLabel: 'Els gifs més populars',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line',
+    },
+    {
+      label: 'Cerca',  
+      subLabel: 'Cerca els teus gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass',
+    },
+  ];
 }
---
+++ 03-gifs-app/src/app/gifs/interfaces/menu-option.interface.ts
@@ -0,0 +1,7 @@
+export interface MenuOption {
+    label: string;
+    subLabel: string;
+    route: string;
+    icon: string;
+}
+
